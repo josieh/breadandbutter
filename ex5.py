905a056b4704a7bdfc80ec4e17a6e0bd5a4a4998
josieh <hollingsworth.josie@gmail.com>
2014-01-18 18:23:11 -0500

    adding this again

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = "josie hollingsworth"
+my_age = 21
+my_height = 63
+my_weight = 123
+my_eyes = "brown"
+my_teeth = "sparkling"
+my_hair = 'brown'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "actually that's not too heavy"
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+
+#This line is tricky, try to get it right, biotch
+
+print "If I add %d, %d, and %d I get %d." % (my-age, my_height, my_weight, my_age + my_weight)
