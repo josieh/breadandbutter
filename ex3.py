7d2e525f450c763fc17b70002e44bf1f438d7846
josieh <hollingsworth.josie@gmail.com>
2014-01-18 17:49:08 -0500

    This is example 3 from the tutorial

---
+++ ex3.py
@@ -0,0 +1,25 @@
+print "I will now count my chickens:" #this is a description of it
+
+print "hens", 25 + 30 / 6     # this is how many hens there are, 30 divided by 6 plus 25
+print "roosters", 100 - 25 * 3 % 4       #This is how many roosters there are, including the remainder
+
+#the % sign is a Another way to say it is, "X divided by Y with J remaining." For example, "100 divided by 16 with 4 remaining." The result of % is the J part, or the remaining part.
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
